eca32dea1c2e237dbf051e3dc74d7f6a49fc791d
achkatam <agmatevv@gmail.com>
2023-01-10 11:19:53 -0500

    added solution

---
+++ Subqueries and Joins Exercise/HighestPeakAndLongestRiverByCountry.sql
@@ -0,0 +1,19 @@
+--17.	Highest Peak and Longest River by Country
+SELECT TOP(5)
+	c.CountryName,
+	MAX(p.Elevation) AS [HighestPeakElevation],
+	MAX([r].[Length]) AS [LongestRiverLength]
+FROM [Countries] AS c
+LEFT JOIN [CountriesRivers] AS cr 
+		ON cr.CountryCode = c.CountryCode
+LEFT JOIN [Rivers] AS r
+		ON cr.RiverId = r.Id
+LEFT JOIN [MountainsCountries] AS mc
+		ON mc.CountryCode = c.CountryCode
+LEFT JOIN [Mountains] AS m
+		ON m.Id = mc.MountainId
+LEFT JOIN [Peaks] AS p
+		ON p.MountainId = m.Id
+GROUP BY c.CountryName
+ORDER BY [HighestPeakElevation] DESC, [LongestRiverLength] DESC
+		, c.CountryName
