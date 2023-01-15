9a27b0655a05ed8b391efb07123edb62394d2009
achkatam <agmatevv@gmail.com>
2023-01-15 12:55:50 -0500

    added solution

---
+++ Indices and Data Aggregation/AgeGroups.sql
@@ -0,0 +1,16 @@
+SELECT 
+	AgeGroup,
+	COUNT(*) AS WizardCount
+  FROM(
+SELECT 
+	CASE 
+		WHEN wz.Age BETWEEN 0 AND 10 THEN '[0-10]'
+		WHEN wz.Age BETWEEN  11 AND 20 THEN '[11-20]'
+		WHEN wz.Age BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN wz.Age BETWEEN  31 AND 40 THEN '[31-40]'
+		WHEN wz.Age BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN wz.Age BETWEEN  51 AND 60 THEN  '[51-60]'
+		WHEN wz.Age >= 61 THEN '[61+]'
+		END AS AgeGroup
+FROM WizzardDeposits AS wz) AS AgeGroup
+GROUP BY AgeGroup
