2fe20a98780a21b1cb19203bee3d40434097f71b
achkatam <agmatevv@gmail.com>
2023-01-19 23:03:25 -0500

    added solution

---
+++ Exam Practice/13 February 21'/12.SearchForFiles.sql
@@ -0,0 +1,15 @@
+--delete ALTER before submitting in judge
+CREATE OR ALTER PROC usp_SearchForFiles(@fileExtension VARCHAR(10))
+AS
+BEGIN
+			SELECT 
+			f.Id,
+			f.[Name],
+			CONCAT(f.Size,'','KB') AS Size
+		FROM Files AS f
+		WHERE f.[Name] LIKE CONCAT('%','',@fileExtension)
+END
+
+GO
+
+EXEC usp_SearchForFiles 'txt'
