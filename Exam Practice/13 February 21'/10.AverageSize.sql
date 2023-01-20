dd4e59c799db3e179a63645c4b18b293fd12884d
achkatam <agmatevv@gmail.com>
2023-01-19 22:48:35 -0500

    added solution

---
+++ Exam Practice/13 February 21'/10.AverageSize.sql
@@ -0,0 +1,9 @@
+SELECT 
+	u.[Username],
+	AVG(f.Size) AS Size
+FROM Users AS u
+JOIN RepositoriesContributors AS rc ON u.Id = rc.ContributorId
+JOIN Commits AS c ON c.ContributorId = u.Id
+JOIN Files AS f ON f.CommitId = c.Id
+GROUP BY u.Username
+ORDER BY AVG(f.Size) DESC, u.Username
