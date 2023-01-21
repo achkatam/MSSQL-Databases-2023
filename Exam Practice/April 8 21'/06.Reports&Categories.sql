588b39725a06f4ba262351a02c2b6b3d3ead94f9
achkatam <agmatevv@gmail.com>
2023-01-21 14:35:28 -0500

    added solution

---
+++ Exam Practice/April 8 21'/06.Reports&Categories.sql
@@ -0,0 +1,7 @@
+SELECT 
+	r.Description,
+	c.[Name] AS CategoryName
+FROM Reports AS r
+LEFT JOIN Categories AS c ON c.Id = r.CategoryId
+WHERE r.CategoryId IS NOT NULL
+ORDER BY r.[Description], c.[Name]
