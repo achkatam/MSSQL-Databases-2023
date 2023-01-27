48b71b34c002db4415e67425af0f5891269b9165
achkatam <agmatevv@gmail.com>
2023-01-27 13:09:11 -0500

    added solution

---
+++ Exam Practice/10 Aug 22'/04.Delete.sql
@@ -0,0 +1,13 @@
+SELECT 
+	bp.Id,
+	bp.Name
+FROM BonusPrizes AS bp
+
+SELECT *
+FROM TouristsBonusPrizes
+
+DELETE FROM TouristsBonusPrizes
+WHERE BonusPrizeId = 5
+
+DELETE FROM BonusPrizes
+WHERE Id = 5
