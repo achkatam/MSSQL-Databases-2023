3c95a3a3dbf4dab35af636c48bab312bc6b9f47b
achkatam <agmatevv@gmail.com>
2023-01-27 10:07:23 -0500

    added solution

---
+++ Exam Practice/June 27 20'/06.CurrentClients.sql
@@ -0,0 +1,12 @@
+SELECT 
+		CONCAT(c.FirstName, ' ', c.LastName) AS Client,
+		DATEDIFF(DAY, j.IssueDate, '2017-04-24') AS [Days going],
+		j.Status
+FROM Clients AS c
+JOIN Jobs AS j ON j.ClientId = c.ClientId
+WHERE j.Status <> 'Finished' 
+
+----------------
+SELECT 
+	j.IssueDate
+FROM Jobs AS j
